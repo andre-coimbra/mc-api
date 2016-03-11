41c89cadad51e66578be6cf5cac885e4b2304870
pa-andre.coimbra <andre.coimbra@paginasamarelas.pt>
2016-03-11 10:58:22 +0000

    hello world added

---
+++ MC-API/src/pt/paginasamarelas/Tests/HelloWorld.java
@@ -0,0 +1,11 @@
+package pt.paginasamarelas.Tests;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Jenkins!!");
+
+	}
+
+}
